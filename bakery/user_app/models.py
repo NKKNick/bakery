b84f2d9da60b43d0758dc664e04412659267ebd3
Frank <pongsapat.ch.64@ubu.ac.th>
2024-01-24 12:24:27 +0700

    update

--- bakery/user_app/models.py
+++ bakery/user_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from bakery_app.models import Product
 
 # Create your models here.
+class Customer(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    phone = models.IntegerField(max_length=10)
+    address = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class CartDetail(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.IntegerField()
--- bakery/user_app/views.py
+++ bakery/user_app/views.py
@@ -1,3 +1,6 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(req):
+    return HTTPResponse("asdawdawd")
