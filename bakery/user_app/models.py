eeff7e2548fa0cbf1f2ceff3439fac7ef230dc9b
Nick <supawit.th.64@ubu.ac.th>
2024-03-04 10:23:42 +0700

    fix UserCustomer

--- bakery/user_app/models.py
+++ bakery/user_app/models.py
@@ -7,7 +7,7 @@ class Customer(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
+    address = models.CharField(max_length=255)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     def __str__(self):
         return f'{self.firstname}'
